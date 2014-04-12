732847d16e6831d59d8eaec811a714eed194c64b
Jed Hastwell <jed@whistlefire.com>
2014-04-12 16:24:44 +0200

    Spawn halos. Setup contact between cannon balls and halos.

--- Space Cannon/CCMyScene.m
+++ Space Cannon/CCMyScene.m
@@ -15,7 +15,14 @@
     BOOL _didShoot;
 }
 
-static const CGFloat SHOOT_SPEED = 1000.0f;
+static const CGFloat kCCShootSpeed = 1000.0;
+static const CGFloat kCCHaloLowAngle = 200.0 * M_PI / 180.0;
+static const CGFloat kCCHaloHighAngle = 340.0 * M_PI / 180.0;
+static const CGFloat kCCHaloSpeed = 100.0;
+
+static const uint32_t kCCHaloCategory = 0x1 << 0;
+static const uint32_t kCCBallCategory = 0x1 << 1;
+static const uint32_t kCCEdgeCategory = 0x1 << 2;
 
 static inline CGVector radiansToVector(CGFloat radians)
 {
@@ -25,12 +32,18 @@ static inline CGVector radiansToVector(CGFloat radians)
     return vector;
 }
 
+static inline CGFloat randomInRange(CGFloat low, CGFloat high)
+{
+    CGFloat value = arc4random_uniform(UINT32_MAX) / (CGFloat)UINT32_MAX;
+    return value * (high - low) + low;
+}
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         
         // Turn off gravity.
         self.physicsWorld.gravity = CGVectorMake(0.0, 0.0);
+        self.physicsWorld.contactDelegate = self;
         
         // Add background.
         SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"Starfield"];
@@ -43,11 +56,13 @@ static inline CGVector radiansToVector(CGFloat radians)
         SKNode *leftEdge = [[SKNode alloc] init];
         leftEdge.physicsBody = [SKPhysicsBody bodyWithEdgeFromPoint:CGPointZero toPoint:CGPointMake(0.0, self.size.height)];
         leftEdge.position = CGPointZero;
+        leftEdge.physicsBody.categoryBitMask = kCCEdgeCategory;
         [self addChild:leftEdge];
         
         SKNode *rightEdge = [[SKNode alloc] init];
         rightEdge.physicsBody = [SKPhysicsBody bodyWithEdgeFromPoint:CGPointZero toPoint:CGPointMake(0.0, self.size.height)];
         rightEdge.position = CGPointMake(self.size.width, 0.0);
+        rightEdge.physicsBody.categoryBitMask = kCCEdgeCategory;
         [self addChild:rightEdge];
         
         
@@ -65,6 +80,11 @@ static inline CGVector radiansToVector(CGFloat radians)
                                                       [SKAction rotateByAngle:-M_PI duration:2]]];
         [_cannon runAction:[SKAction repeatActionForever:rotateCannon]];
         
+        // Create spawn halo actions.
+        SKAction *spawnHalo = [SKAction sequence:@[[SKAction waitForDuration:2 withRange:1],
+                                                   [SKAction performSelector:@selector(spawnHalo) onTarget:self]]];
+        [self runAction:[SKAction repeatActionForever:spawnHalo]];
+        
     }
     return self;
 }
@@ -81,13 +101,53 @@ static inline CGVector radiansToVector(CGFloat radians)
     [_mainLayer addChild:ball];
     
     ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:6.0];
-    ball.physicsBody.velocity = CGVectorMake(rotationVector.dx * SHOOT_SPEED, rotationVector.dy * SHOOT_SPEED);
+    ball.physicsBody.velocity = CGVectorMake(rotationVector.dx * kCCShootSpeed, rotationVector.dy * kCCShootSpeed);
     ball.physicsBody.restitution = 1.0;
     ball.physicsBody.linearDamping = 0.0;
     ball.physicsBody.friction = 0.0;
+    ball.physicsBody.categoryBitMask = kCCBallCategory;
+    ball.physicsBody.collisionBitMask = kCCEdgeCategory;
     
 }
 
+-(void)spawnHalo
+{
+    // Create halo node.
+    SKSpriteNode *halo = [SKSpriteNode spriteNodeWithImageNamed:@"Halo"];
+    halo.position = CGPointMake(randomInRange(halo.size.width * 0.5, self.size.width - (halo.size.width * 0.5)),
+                                self.size.height + (halo.size.height * 0.5));
+    halo.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:16.0];
+    CGVector direction = radiansToVector(randomInRange(kCCHaloLowAngle, kCCHaloHighAngle));
+    halo.physicsBody.velocity = CGVectorMake(direction.dx * kCCHaloSpeed, direction.dy * kCCHaloSpeed);
+    halo.physicsBody.restitution = 1.0;
+    halo.physicsBody.linearDamping = 0.0;
+    halo.physicsBody.friction = 0.0;
+    halo.physicsBody.categoryBitMask = kCCHaloCategory;
+    halo.physicsBody.collisionBitMask = kCCEdgeCategory;
+    halo.physicsBody.contactTestBitMask = kCCBallCategory;
+    [_mainLayer addChild:halo];
+}
+
+-(void)didBeginContact:(SKPhysicsContact *)contact
+{
+    SKPhysicsBody *firstBody;
+    SKPhysicsBody *secondBody;
+    
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask) {
+        firstBody = contact.bodyA;
+        secondBody = contact.bodyB;
+    } else {
+        firstBody = contact.bodyB;
+        secondBody = contact.bodyA;
+    }
+    
+    if (firstBody.categoryBitMask == kCCHaloCategory && secondBody.categoryBitMask == kCCBallCategory) {
+        // Collision between halo and ball.
+        [firstBody.node removeFromParent];
+        [secondBody.node removeFromParent];
+    }
+    
+}
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     
