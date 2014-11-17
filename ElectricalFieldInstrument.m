e42fb03d2ca139c955a497a909314842fb479f76
Nicholas Arner <nicholasarner@gmail.com>
2014-11-17 12:49:46 -0500

    Added instrument for Ball Hits Halo event Removed any reference to "DeepCrunch" Instrument

---
+++ ElectricalFieldInstrument.m
@@ -0,0 +1,13 @@
+//
+//  ElectricalFieldInstrument.m
+//  Space Cannon
+//
+//  Created by Nicholas Arner on 11/17/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "ElectricalFieldInstrument.h"
+
+@implementation ElectricalFieldInstrument
+
+@end
