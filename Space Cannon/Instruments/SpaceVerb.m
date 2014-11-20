b3f86b6da217f72b9197727e706b21700e7f4d12
Nicholas Arner <nicholasarner@gmail.com>
2014-11-20 16:12:50 -0500

    Removed delay

--- Space Cannon/Instruments/SpaceVerb.m
+++ Space Cannon/Instruments/SpaceVerb.m
@@ -22,22 +22,14 @@
         
         _feedbackLevel = [[AKInstrumentProperty alloc] initWithValue:0.8 minimum:0.0 maximum:0.95];
         [self addProperty:_feedbackLevel];
-        
+
         AKSum *leftSum = [[AKSum alloc] initWithOperands:softBoing.leftOutput, crunch, buzz.leftOutput, laser.leftOutput, zwoop.leftOutput, siren.leftOutput, nil];
         [self connect:leftSum];
         
         AKSum *rightSum = [[AKSum alloc] initWithOperands:softBoing.rightOutput, crunch, buzz.rightOutput, laser.rightOutput, zwoop.rightOutput, siren.rightOutput, nil];
         [self connect:rightSum];
         
-        AKDelay *leftDelay = [[AKDelay alloc] initWithAudioSource:leftSum delayTime:akp(1.0)];
-        [leftDelay setOptionalFeedback:akp(0.9)];
-        [self connect:leftDelay];
-        
-        AKDelay *rightDelay = [[AKDelay alloc] initWithAudioSource:rightSum delayTime:akp(1.5)];
-        [leftDelay setOptionalFeedback:akp(0.95)];
-        [self connect:rightDelay];
-        
-        AKStereoAudio *stereoSum = [[AKStereoAudio alloc] initWithLeftAudio:leftDelay rightAudio:rightDelay];
+        AKStereoAudio *stereoSum = [[AKStereoAudio alloc] initWithLeftAudio:leftSum rightAudio:rightSum];
         
         AKReverb *reverb = [[AKReverb alloc] initWithSourceStereoAudio:[stereoSum scaledBy:akp(0.33)]
                                                          feedbackLevel:_feedbackLevel
